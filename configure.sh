c88133b83dfdb981233a8c472d0eacdc097618b6
Eric Bishard <e.bishard@gmail.com>
2020-01-23 15:13:18 -0800

    change storageMode to default

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1q
 
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=$CB_ADMIN_USER -d password=$CB_ADMIN_PASS 
 
-curl -v -u $CB_ADMIN_USER:$CB_ADMIN_PASS -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl -v -u $CB_ADMIN_USER:$CB_ADMIN_PASS -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=default'
 
 curl -v -u $CB_ADMIN_USER:$CB_ADMIN_PASS -X POST http://127.0.0.1:8091/pools/default/buckets -d name=$CB_BUCKET -d bucketType=couchbase -d ramQuotaMB=128 -d authType=sasl -d saslPassword=$CB_BUCKET_PASS
 
